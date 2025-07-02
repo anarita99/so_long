7708d33ac2d1629aea1426c536bd79f0b43167a4
Ana Rita Dores <adores@c1r9s5.42lisboa.com>
2025-07-02 16:40:01 +0100

    OK

--- cat.c
+++ cat.c
@@ -6,27 +6,14 @@
 /*   By: adores <adores@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 14:03:58 by adores            #+#    #+#             */
-/*   Updated: 2025/07/02 12:25:29 by adores           ###   ########.fr       */
+/*   Updated: 2025/07/02 16:29:38 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-/// @brief faz a animacao do gato
-/// @param game frames
-/// @return nao retorna
-int	animate_cat(t_game *game)
-{
-	int			tile_size;
-	static int	f;
-	static int	counter;
-	int			x;
-	int			y;
 
-	tile_size = 64;
-	x = game->cat_x * tile_size;
-	y = game->cat_y * tile_size;
-	mlx_clear_window(game->mlx, game->win);
-	draw_map(game);
+static void draw_cat_sprite(t_game *game, int f, int x, int y)
+{
 	if (game->walk_timer > 0)
 	{
 		if (game->cat_dir == CAT_LEFT)
@@ -40,11 +27,25 @@ int	animate_cat(t_game *game)
 		game->walk_timer--;
 	}
 	else if (game->map[game->cat_y][game->cat_x] == 'X')
-	{
 		mlx_put_image_to_window(game->mlx, game->win, game->exit2, x, y);
-	}
 	else
 		mlx_put_image_to_window(game->mlx, game->win, game->idle[f], x, y);
+}
+
+int	animate_cat(t_game *game)
+{
+	int			tile_size;
+	static int	f;
+	static int	counter;
+	int			x;
+	int			y;
+
+	tile_size = 64;
+	x = game->cat_x * tile_size;
+	y = game->cat_y * tile_size;
+	mlx_clear_window(game->mlx, game->win);
+	draw_map(game);
+	draw_cat_sprite(game, f, x, y);
 	counter++;
 	if (counter > 4500)
 	{
